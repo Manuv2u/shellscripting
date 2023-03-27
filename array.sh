ed62231b160438c76b133938cb1c6b882564aeab
EC2 Default User <ec2-user@ip-172-31-39-50.ap-south-1.compute.internal>
2023-03-27 08:17:57 +0000

    new files from array

---
+++ array.sh
@@ -0,0 +1,5 @@
+echo "please enter the devopstools"
+read -a DevOpstools
+echo "the Devopstools which you have typed are: " ${DevOpstools[*]}
+
+echo "the Devopstools 5th tool is : " ${DevOpstools[4]} 
---
+++ array3.sh
@@ -0,0 +1,5 @@
+echo "1st number"
+read num1
+echo "2nd number"
+read num2
+echo "sum of two numbers is : "`expr $num1 + $num2`
